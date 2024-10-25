41b4387387b1973813f12b356feea02955c11880
Kocin <mail.jdnielss@gmail.com>
2024-10-25 19:11:21 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        log.Println("Handling request to Root")
         fmt.Fprintf(w, "Hello, World!")
     })
 
     http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "OK")
+        log.Println("Handling request to Health")
+        fmt.Fprintf(w, "OK, Health")
     })
 
     log.Println("Starting server on :8080")
+    log.Println(http)
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatalf("could not start server: %s\n", err)
     }
